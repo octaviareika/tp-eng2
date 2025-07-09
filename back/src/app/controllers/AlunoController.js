1a3f605d71495d2c9339d02bb3d318eff3c39352
yasminaa18 <yasminaltino8@gmail.com>
2025-07-08 22:21:22 -0300

    login e registro funcionando, arquivos js agora na pasta dist

--- back/src/app/controllers/AlunoController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AlunoController = void 0;
-class AlunoController {
-}
-exports.AlunoController = AlunoController;
