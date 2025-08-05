7caf1aee85bfc503c61d559d5c891b7747ac1d90
Camila Aparecida <milow.amu@gmail.com>
2025-08-05 09:13:46 -0300

    Testando

--- back/src/database/data-source.ts
+++ back/src/database/data-source.ts
@@ -9,6 +9,8 @@ import path from "path";
 
 dotenv.config();
 
+console.log('DB_PASSWORD do .env:', process.env.DB_PASSWORD);
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
