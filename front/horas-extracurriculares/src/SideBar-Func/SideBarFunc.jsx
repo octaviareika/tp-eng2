e280648a1babf631a08fff58be6f8c8a9f24fb51
TLiuth <73227372+TLiuth@users.noreply.github.com>
2025-08-19 16:48:17 -0300

    Revert "Merge branch '52-front---adicionar-nome-do-aluno-no-lugar-de-meu-perfil' into development"
    This reverts commit b39aedc2461640a76f80e925b0eed1141c3768e1, reversing
    changes made to fb889c953cc9c2266dbe56b879b5b325ac8274fa.
    
    Defective branch

--- front/horas-extracurriculares/src/SideBar-Func/SideBarFunc.jsx
+++ front/horas-extracurriculares/src/SideBar-Func/SideBarFunc.jsx
@@ -1,24 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import fotoPerfil from '../assets/icone-perfil.png';
 import logoUfop from '../assets/logo-ufop.png';
 import { Link } from "react-router-dom";
 
-
-const SidebarFunc = () => {
-  const [userName, setUserName] = useState ('');
-
-  useEffect(() =>{
-    fetch('/api/user/profile', { credentials: 'include' })
-      .then(response => response.json())
-      .then(data => setUserName(data.name))
-      .catch(() => setUserName('Meu Perfil'));
-  }, []);
-
+const SidebarFunc = () => (
   <div className="sidebar">
     <div className="painelAluno">Painel do Professor</div>
     <div className="profile">
       <img className="size-icon" src={fotoPerfil} alt="Foto de perfil da UFOP" />
-      <span className="my-profile">{userName || 'Meu Perfil'}</span>
+      <span className="my-profile">Meu Perfil</span>
     </div>
     <ul>
       <li className="list"><Link to="/funcionario">Atividades Pendentes</Link></li>
@@ -30,6 +20,6 @@ const SidebarFunc = () => {
       <img className="logo-ufop" src={logoUfop} alt="Logo da UFOP" />
     </div>
   </div>
-};
+);
 
 export default SidebarFunc;
